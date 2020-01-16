541c81c5df68d68025b0f87ced7b51071590108a
Yannis Roussos <iroussos@gitlab.com>
2020-01-16 23:58:45 +0200

    Add models for clicks per XXX reports

---
+++ models/transform/fb_clicks_per_day_age.sql
@@ -0,0 +1,41 @@
+WITH ads_insights as (
+
+     select *
+     from {{ref('fb_ads_insights_age_and_gender')}}
+
+)
+
+select
+
+  campaign_name,
+  adset_name,
+  ad_name,
+  insights_date,
+  age,
+
+  CONCAT (insights_date, ' | ', age, ' | ', ad_name, ' | ', adset_name, ' | ', campaign_name) as label, 
+  
+  SUM(clicks) as clicks, 
+  
+  round((1.0 * SUM(clicks) / NULLIF(SUM(impressions), 0))  * 100, 2) as ctr,
+
+  round(SUM(spend) / NULLIF(SUM(clicks), 0), 2) as cpc
+
+from ads_insights
+
+group by 
+
+  campaign_name,
+  adset_name,
+  ad_name,
+  insights_date,
+  age
+
+having SUM(clicks) > 0
+
+order by 
+  insights_date, 
+  age, 
+  ad_name, 
+  adset_name,
+  campaign_name
---
+++ models/transform/fb_clicks_per_day_device.sql
@@ -0,0 +1,41 @@
+WITH ads_insights as (
+
+     select *
+     from {{ref('fb_ads_insights_platform_and_device')}}
+
+)
+
+select
+
+  campaign_name,
+  adset_name,
+  ad_name,
+  insights_date,
+  impression_device as device,
+
+  CONCAT (insights_date, ' | ', impression_device, ' | ', ad_name, ' | ', adset_name, ' | ', campaign_name) as label, 
+  
+  SUM(clicks) as clicks, 
+  
+  round((1.0 * SUM(clicks) / NULLIF(SUM(impressions), 0))  * 100, 2) as ctr,
+
+  round(SUM(spend) / NULLIF(SUM(clicks), 0), 2) as cpc
+
+from ads_insights
+
+group by 
+
+  campaign_name,
+  adset_name,
+  ad_name,
+  insights_date,
+  impression_device
+
+having SUM(clicks) > 0
+
+order by 
+  insights_date, 
+  impression_device, 
+  ad_name, 
+  adset_name,
+  campaign_name
