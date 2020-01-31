3a85918f5a83821f2c6ad0a41e83642a2612803e
Yannis Roussos <iroussos@gitlab.com>
2020-01-31 23:53:19 +0200

    Update the results calculations to only support Objectives that we know about or return NULL otherwise

--- macros/ads_insights_metrics.sql
+++ macros/ads_insights_metrics.sql
@@ -17,35 +17,53 @@
         canvas_avg_view_percent as canvas_avg_view_percent,
         canvas_avg_view_time as canvas_avg_view_time,
 
+        -- Calculate the results for this Ad based on the Objective:
+        -- 1. Iterate through the actions jsonb array and check each included hash (action_elements)
+        -- 2. For the Objectives we know what to do with and depending on the objective
+        --    match the action_elements.action_type with the appropriate string or set of strings
+        -- 3. If you found anything, SUM all the action_elements.value for the action_elements that matched
+        -- 4. If you found nothing, return a 0.0 for the Objectives we support or a NULL for the ones we do not
         coalesce(
-        (
-            SELECT
-                SUM((action_elements::json->>'value')::numeric)
-            FROM
-                jsonb_array_elements(actions) action_elements
-            WHERE
-                case
-                    when objective = 'LINK_CLICKS'
-                        then action_elements::json->>'action_type' = 'link_click'
-                    when objective = 'PAGE_LIKES'
-                        then action_elements::json->>'action_type' = 'like'
-                    when objective = 'POST_ENGAGEMENT'
-                        then action_elements::json->>'action_type' = 'post_engagement'
-                    when objective = 'APP_INSTALLS'
-                        -- app_install, mobile_app_install, omni_app_install
-                        then action_elements::json->>'action_type' like '%app_install'
-                    when objective = 'EVENT_RESPONSES'
-                        then action_elements::json->>'action_type' = 'rsvp'
-                    when objective = 'VIDEO_VIEWS'
-                        then action_elements::json->>'action_type' = 'video_view'
-                    when objective = 'LEAD_GENERATION'
-                        then action_elements::json->>'action_type' = 'lead'
-                    when objective = 'MESSAGES'
-                        then action_elements::json->>'action_type' = 'onsite_conversion.messaging_conversation_started_7d'
-                    else action_elements::json->>'action_type' =
-                         substring(lower(objective) from 1 for length(objective) - 1)
-                end
-        ), 0.0) AS results,
+            (
+                SELECT
+                    SUM((action_elements::json->>'value')::numeric)
+                FROM
+                    jsonb_array_elements(actions) action_elements
+                WHERE
+                    case
+                        when objective = 'LINK_CLICKS'
+                            then action_elements::json->>'action_type' = 'link_click'
+                        when objective = 'PAGE_LIKES'
+                            then action_elements::json->>'action_type' = 'like'
+                        when objective = 'POST_ENGAGEMENT'
+                            then action_elements::json->>'action_type' = 'post_engagement'
+                        when objective = 'APP_INSTALLS'
+                            -- app_install, mobile_app_install, omni_app_install
+                            then action_elements::json->>'action_type' like '%app_install'
+                        when objective = 'EVENT_RESPONSES'
+                            then action_elements::json->>'action_type' = 'rsvp'
+                        when objective = 'VIDEO_VIEWS'
+                            then action_elements::json->>'action_type' = 'video_view'
+                        when objective = 'LEAD_GENERATION'
+                            then action_elements::json->>'action_type' = 'lead'
+                        when objective = 'MESSAGES'
+                            then action_elements::json->>'action_type' = 'onsite_conversion.messaging_conversation_started_7d'
+                    end
+            ),
+
+            -- When we do not know how to calculate the results for an objective,
+            -- notify consumers of this table with a NULL
+            case
+                when objective in (
+                        'LINK_CLICKS', 'PAGE_LIKES', 'POST_ENGAGEMENT', 'APP_INSTALLS',
+                        'EVENT_RESPONSES', 'VIDEO_VIEWS', 'LEAD_GENERATION', 'MESSAGES'
+                    )
+                    then 0.0
+                else
+                    NULL
+            end
+
+        ) AS results,
 
         nullif(objective,'') as objective,
 
