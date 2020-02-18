586dc4816d56410ae7e3eda93d42cc852f2035ac
Yannis Roussos <iroussos@gitlab.com>
2020-02-18 12:19:54 +0200

    Add Funnel Costs and Funnel Performance per month transform models

---
+++ models/transform/fb_funnel_costs_per_month.sql
@@ -0,0 +1,59 @@
+with ads_insights as (
+
+     select *
+     from {{ref('fb_ads_insights')}}
+
+)
+
+select
+
+  account_name,
+  campaign_name,
+  adset_name,
+  ad_name,
+
+  -- The start of the month this report is for
+  to_char(MIN(insights_date), 'YYYY-MM') as month,
+  date_trunc('month', MIN(insights_date))::date as month_start,
+
+  -- Generate a descriptive label: "2020-02 | Ad name | Adset | Campaign"
+  CONCAT
+  (
+    to_char(MIN(insights_date), 'YYYY-MM'), ' | ',
+    ad_name, ' | ', adset_name, ' | ', campaign_name, ' | ', account_name
+  ) as label,
+
+  SUM(impressions) as impressions,
+
+  round((SUM(spend) / NULLIF(SUM(impressions), 0))  * 1000, 2) as cpm,
+
+  SUM(clicks) as clicks,
+
+  round(SUM(spend) / NULLIF(SUM(clicks), 0), 2) as cpc,
+
+  SUM(results) as results,
+
+  -- Cost Per Result: Spend / Results
+  round(SUM(spend) / NULLIF(SUM(results), 0), 2) as cost_per_result,
+
+  SUM(spend) as spend
+
+from ads_insights
+
+group by
+  insights_year,
+  insights_month,
+  account_name,
+  campaign_name,
+  adset_name,
+  ad_name
+
+having SUM(impressions) > 0
+
+order by
+  insights_year,
+  insights_month,
+  ad_name,
+  adset_name,
+  campaign_name,
+  account_name
---
+++ models/transform/fb_funnel_performance_per_month.sql
@@ -0,0 +1,54 @@
+with ads_insights as (
+
+     select *
+     from {{ref('fb_ads_insights')}}
+
+)
+
+select
+
+  account_name,
+  campaign_name,
+  adset_name,
+  ad_name,
+
+  -- The start of the month this report is for
+  to_char(MIN(insights_date), 'YYYY-MM') as month,
+  date_trunc('month', MIN(insights_date))::date as month_start,
+
+  -- Generate a descriptive label: "2020-02 | Ad name | Adset | Campaign"
+  CONCAT
+  (
+    to_char(MIN(insights_date), 'YYYY-MM'), ' | ',
+    ad_name, ' | ', adset_name, ' | ', campaign_name, ' | ', account_name
+  ) as label,
+
+  SUM(impressions) as impressions,
+  SUM(reach) as reach,
+  round(1.0 * SUM(impressions) / NULLIF(SUM(reach), 0), 2) as frequency,
+
+  SUM(clicks) as clicks,
+  round((1.0 * SUM(clicks) / NULLIF(SUM(impressions), 0)) * 100, 2) as ctr,
+
+  SUM(results) as results,
+
+  -- Result Rate from Click: ((Results / Click) * 100) %
+  round((1.0 * SUM(results) / NULLIF(SUM(clicks), 0)) * 100, 2) as result_rate_from_clicks
+
+from ads_insights
+
+group by
+  insights_year,
+  insights_month,
+  account_name,
+  campaign_name,
+  adset_name,
+  ad_name
+
+order by
+  insights_year,
+  insights_month,
+  ad_name,
+  adset_name,
+  campaign_name,
+  account_name
